c5110073c6064d3fae8cea4ac1a77ad18833f7ca
AnastasiyaSoloveva <nastya-soloveva@mail.ru>
2017-07-03 13:59:05 +0300

    добавлено дз с матрицей

---
+++ src/homework/matrix/Matrix.java
@@ -0,0 +1,58 @@
+package homework.matrix;
+
+import java.util.Random;
+
+public class Matrix {
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {randomMatrixElement(), randomMatrixElement(), randomMatrixElement(),},
+                {randomMatrixElement(),randomMatrixElement(),randomMatrixElement()},
+                {randomMatrixElement(),randomMatrixElement(),randomMatrixElement(),},
+                {randomMatrixElement(),randomMatrixElement(),randomMatrixElement()},
+                {randomMatrixElement(),randomMatrixElement(),randomMatrixElement(),}
+        };
+        System.out.println("Сгенерированная матрица: ");
+
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] +" ");
+
+            }
+            System.out.println();
+        }
+
+        countSum(matrix);
+
+
+    }
+
+    private static void countSum(int[][] matrix) {
+        int sum = 0;
+       OUTER: for (int i = 0; i < matrix.length; i++) {
+            int[] row = matrix[i];
+            for (int j = 0; j < row.length; j++) {
+                if (row[j] < 0) {
+                    sum = 0;
+                    continue OUTER;
+                }
+                sum += row[j];
+
+            }
+            System.out.println("Сумма элементов " + i + " ряда: " +sum);
+        }
+
+    }
+
+    private static int randomMatrixElement(){
+        Random random = new Random();
+
+        int matrixEl = random.nextInt(21) - 10; //случайный числа от -10 до 10
+
+        return matrixEl;
+    }
+
+
+}
+
