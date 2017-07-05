8005cb9592e720edeac00bfb61434a31ed3201c4
AnastasiyaSoloveva <nastya-soloveva@mail.ru>
2017-07-05 19:11:53 +0300

    вывод массива на печать

---
+++ src/classwork/lesson170704/PrintArray.java
@@ -0,0 +1,34 @@
+package classwork.lesson170704;
+
+
+import java.util.Random;
+
+public class PrintArray {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int m = random.nextInt(20);
+        int[] array = new int[m];
+        for (int i = 0; i < array.length ; i++) {
+            array[i] = random.nextInt(100);
+
+        }
+
+        printArray(array);
+
+    }
+
+
+    // вывод массива на печать в виде  [0, 1, 2]
+    private static void printArray(int[] array) {
+        System.out.print("[");
+        for (int i = 0; i < array.length; i ++) {
+            if (i == array.length-1){
+                System.out.println(array[array.length-1] + "]");
+                break;
+
+            }
+            System.out.print(array[i] + ", ");
+        }
+    }
+}
