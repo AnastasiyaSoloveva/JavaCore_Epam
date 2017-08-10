4b9505ca268d0ad8e4440d4afc9c2d9b61e6f47b
AnastasiyaSoloveva <nastya-soloveva@mail.ru>
2017-08-10 20:55:43 +0300

    add generics

---
+++ src/classwork/lesson170809/UseDictionary.java
@@ -0,0 +1,41 @@
+package classwork.lesson170809;
+
+public class UseDictionary {
+    public static void main(String[] args) {
+
+        Dictionary d = new Dictionary();
+        d.put("Pete","NY");
+        d.put("John","Boston");
+        d.put("Vasya","Moscow");
+
+
+        System.out.println(d.get("Pete"));
+        System.out.println(d.get("John"));
+        System.out.println(d.get("Vasya"));
+
+        d.put("Vasya", "SPB");
+        System.out.println(d.get("Vasya"));
+
+        for (Dictionary.Pair pair: d) {
+            System.out.println(pair.key + ": " + pair.value);
+
+        }
+
+
+        Dictionary d1 = new Dictionary();
+        d1.put(2,6);
+        d1.put("Anna", 456);
+        d1.put("Bob","Murka");
+
+
+        for (Dictionary.Pair pair: d1) {
+            System.out.println(pair.key + ": " + pair.value);
+
+        }
+
+
+
+
+
+    }
+}
